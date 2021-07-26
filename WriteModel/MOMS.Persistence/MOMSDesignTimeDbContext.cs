9d782dc206dccd87618c67eb98b068417135cfa1
Amir Ahmadi <ahmadi.amirhossien@gmail.com>
2021-07-27 00:55:47 +0430

    connectionstring

--- WriteModel/MOMS.Persistence/MOMSDesignTimeDbContext.cs
+++ WriteModel/MOMS.Persistence/MOMSDesignTimeDbContext.cs
@@ -13,8 +13,8 @@ namespace MOMS.Persistence
         public MOMSDbContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<MOMSDbContext>();
-
-            optionsBuilder.UseSqlServer("Server =.,1433;Data Source=.;Database = MOMS_Developer;Integrated Security=true;");
+           
+            optionsBuilder.UseSqlServer("data source= 185.55.224.3 ;Initial Catalog=dahriman_MOMS  ;User Id=dahriman_DevADUser ;password=@N379645099_A_M ;");
             return new MOMSDbContext(optionsBuilder.Options);
         }
     }
